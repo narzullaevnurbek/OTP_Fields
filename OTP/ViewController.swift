528a39bae197cc312634aee75626842fcbd5e977
narzullaevnurbek <narzullaevnurbekdev@gmail.com>
2023-11-28 14:53:24 +0500

    Initial Commit

---
+++ OTP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTP
+//
+//  Created by Narzullaev Nurbek on 28/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
